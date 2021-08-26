d8a932a30a88e44df9859c8e289c7986abb1ea41
KAVINDYADEVINDI <kavindyadewindi12345678@gmail.com>
2021-08-25 22:21:04 -0700

    connect firebase

--- todo/src/app/app.module.ts
+++ todo/src/app/app.module.ts
@@ -6,16 +6,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CourseComponent } from './course.component';
 
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment';
+
 @NgModule({
-  declarations: [
-    AppComponent,CourseComponent
-  ],
+  declarations: [AppComponent, CourseComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
   ],
   //add dependencies
   providers: [CourseService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- todo/src/environments/environment.ts
+++ todo/src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyAn1DHZsNbLOZ9vW-hJ554hTS4As51wiyM",
+    authDomain: "angular-todolist-a9c7d.firebaseapp.com",
+    databaseURL: "https://angular-todolist-a9c7d-default-rtdb.firebaseio.com",
+    projectId: "angular-todolist-a9c7d",
+    storageBucket: "angular-todolist-a9c7d.appspot.com",
+    messagingSenderId: "615412296722",
+    appId: "1:615412296722:web:b46b1864bc9a7cec407ccb",
+    measurementId: "G-CERH0YY6B5"
+  }
 };
 
 /*
